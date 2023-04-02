246a150e7e2b1ccd69062e60a57b617962078344
mmeerrkkeell <127442610+mmeerrkkeell@users.noreply.github.com>
2023-04-02 15:17:49 +0300

    www.patika.dev
    www.patika.dev proje-1

---
+++ patika/patika.py
@@ -0,0 +1,28 @@
+Proje 1
+[22,27,16,2,18,6] -> Insertion Sort
+Yukarı verilen dizinin sort türüne göre aşamalarını yazınız.
+Big-O gösterimini yazınız.
+Time Complexity: Dizi sıralandıktan sonra 18 sayısı aşağıdaki case'lerden hangisinin kapsamına girer? Yazınız
+Average case: Aradığımız sayının ortada olması
+Worst case: Aradığımız sayının sonda olması
+Best case: Aradığımız sayının dizinin en başında olması.
+.
+[7,3,5,8,2,9,4,15,6] dizisinin Selection Sort'a göre ilk 4 adımını yazınız.
+çözüm:
+1-[22,27,16,2,18,6]
+2-[2,27,16,22,18,6]
+3-[2,6,16,22,18,27]
+4-[2,6,16,18,22,27]
+
+big-O gösterimi : (n^2)
+
+18 sayısı avarage case sınıfına girer.
+
+1-[7,3,5,8,2,9,4,15,6]
+2-[2,3,5,8,7,9,4,15,6]
+3-[2,3,4,8,7,9,5,15,6]
+4-[2,3,4,5,7,9,8,15,6]
+5-[2,3,4,5,6,9,8,15,7]
+6-[2,3,4,5,6,7,8,15,9]
+7-[2,3,4,5,6,7,8,9,15]
+
